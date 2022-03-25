6e1851af35398666c8efe3be17c056525450463b
Suthatta <psuthatta@gmail.com>
2022-03-26 00:13:49 +0100

    Updates

---
+++ precache-manifest.a743cc8ced3c3171938bf6404f391a62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9500bd765d55b95fdd52",
+    "url": "/myproducts/static/css/main.fc0cc3cf.chunk.css"
+  },
+  {
+    "revision": "9500bd765d55b95fdd52",
+    "url": "/myproducts/static/js/main.43eab130.chunk.js"
+  },
+  {
+    "revision": "52cd409cf9804b38f8cf",
+    "url": "/myproducts/static/js/runtime~main.b11fae84.js"
+  },
+  {
+    "revision": "7bd782517f55ba8b94d2",
+    "url": "/myproducts/static/css/2.5b12c564.chunk.css"
+  },
+  {
+    "revision": "7bd782517f55ba8b94d2",
+    "url": "/myproducts/static/js/2.f8a655d8.chunk.js"
+  },
+  {
+    "revision": "db569b567d12d48e4e61",
+    "url": "/myproducts/static/js/3.d51c813c.chunk.js"
+  },
+  {
+    "revision": "31cfe652b9741f85d564aad3afb2d14c",
+    "url": "/myproducts/static/media/logo.31cfe652.png"
+  },
+  {
+    "revision": "4fcd624cc978123b1f3be74dc0666017",
+    "url": "/myproducts/static/media/Email.4fcd624c.png"
+  },
+  {
+    "revision": "70e45b13b37d65904ac76cc1b82f1bc3",
+    "url": "/myproducts/index.html"
+  }
+];
